c712acae5ccca7d0c488422a2559fceeca90057a
MeddahAbdellah <meddah.abdellah.spcx@gmail.com>
2022-05-10 18:31:50 +0200

    review(ins) reviewed by @cadesalaberry

--- dist/class/emitter.helper.js
+++ dist/class/emitter.helper.js
@@ -1,34 +1,15 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.EmitterHelper = void 0;
-const forge = __importStar(require("node-forge"));
+const node_forge_1 = require("node-forge");
 class EmitterHelper {
     static getEmitterFromPfx(pfx, passphrase) {
         let organizationalUnit;
-        const p12Der = forge.util.decode64(pfx.toString('base64'));
-        const p12Asn1 = forge.asn1.fromDer(p12Der);
-        const p12 = forge.pkcs12.pkcs12FromAsn1(p12Asn1, false, passphrase);
-        const bags = p12.getBags({ bagType: forge.pki.oids.certBag });
-        const certBag = bags[forge.pki.oids.certBag];
+        const p12Der = node_forge_1.util.decode64(pfx.toString('base64'));
+        const p12Asn1 = node_forge_1.asn1.fromDer(p12Der);
+        const p12 = node_forge_1.pkcs12.pkcs12FromAsn1(p12Asn1, false, passphrase);
+        const bags = p12.getBags({ bagType: node_forge_1.pki.oids.certBag });
+        const certBag = bags[node_forge_1.pki.oids.certBag];
         if (!certBag) {
             throw new Error('Unable to find certBag');
         }
--- src/class/emitter.helper.ts
+++ src/class/emitter.helper.ts
@@ -1,13 +1,13 @@
-import * as forge from 'node-forge';
+import { util, asn1 , pkcs12, pki } from 'node-forge';
 
 export class EmitterHelper {
   public static getEmitterFromPfx(pfx: Buffer, passphrase: string): string {
     let organizationalUnit;
-    const p12Der = forge.util.decode64(pfx.toString('base64'));
-    const p12Asn1 = forge.asn1.fromDer(p12Der);
-    const p12 = forge.pkcs12.pkcs12FromAsn1(p12Asn1, false, passphrase);
-    const bags = p12.getBags({ bagType: forge.pki.oids.certBag });
-    const certBag = bags[forge.pki.oids.certBag];
+    const p12Der = util.decode64(pfx.toString('base64'));
+    const p12Asn1 = asn1.fromDer(p12Der);
+    const p12 = pkcs12.pkcs12FromAsn1(p12Asn1, false, passphrase);
+    const bags = p12.getBags({ bagType: pki.oids.certBag });
+    const certBag = bags[pki.oids.certBag];
     if (!certBag) { throw new Error('Unable to find certBag'); }
     const { cert } = certBag[0];
     if (!cert) { throw new Error('Unable to find a cert in the certBag'); }
