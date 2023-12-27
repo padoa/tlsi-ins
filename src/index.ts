ed510b4e4311e1db4065b092ec02c10160d42a39
Cyril Ghali <cyril.g@padoa-group.com>
2023-12-27 16:52:11 +0100

    export certificate validator class

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './class/emitter.helper';
 export * from './class/insi-person.class';
 export * from './class/lps-context.class';
 export * from './class/lps.class';
+export * from './class/InsCertificateValidator/InsCertificateValidator.class'
 
 export * from './models/insi-fetch-ins.models';
 export * from './models/insi-soap-action.models';
